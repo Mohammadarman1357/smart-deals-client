1a3c566e7a27729f0813447565e41b70dca9636f
Arman <mohammadjahedulislamarman@gmail.com>
2025-12-27 00:45:05 +0600

    added latest products and added fallback function

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Banner from './Banner';
+import Loading from '../pages/Loading';
+import LatestProducts from './LatestProducts/LatestProducts';
+
+const latestProductsPromise = fetch('http://localhost:3000/latest-products').then(res => res.json());
 
 const Home = () => {
     return (
         <div className='max-w-7xl mx-auto bg-base-300'>
             <title>Home</title>
-
             <Banner></Banner>
+
+            <Suspense fallback={<Loading></Loading>}>
+                <LatestProducts latestProductsPromise={latestProductsPromise}></LatestProducts>
+            </Suspense>
 
         </div>
     );
